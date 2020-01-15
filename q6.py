0b73bbd40cbf8574b7d6eade6f746112c60d5eb2
Mahimachouhan <mchouhan@bestpeers.com>
2020-01-15 15:20:08 +0530

    next program

--- q6.py
+++ q6.py
@@ -1,8 +1,6 @@
-import math
+from math import sqrt
 C=50
 H=30
-n=int(input("Enter no. of opertions: "))
-for x in range(0,n)
-	D=int(input(","))
-	Q=math.sqrt((2 * C * D)/H)
-	print(Q,",")
+D=input()
+Q=sqrt((2 * C * D)/H)
+print(Q)
