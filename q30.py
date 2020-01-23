0aa97848e3632c921d52b8f2fb33472bee005777
mahimachouhan <mchouhan@bestpeers.com>
2020-01-23 16:45:13 +0530

    solution of 30th porblem

---
+++ q30.py
@@ -0,0 +1,7 @@
+def concatinate_string(s1,s2):
+	return s1+" "+s2
+
+s1=str(input("Enter string 1:"))
+s2=str(input("Enter string 2:"))
+result=concatinate_string(s1,s2)
+print("concatinated string {0}".format(result))
