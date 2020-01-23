aa531a0c4846fd5be6684b9de73b2c12fc07564e
mahimachouhan <mchouhan@bestpeers.com>
2020-01-23 16:37:57 +0530

    solution of 15th prblem

---
+++ q15.py
@@ -0,0 +1,8 @@
+a=int(input("input= "))
+#a+aa+aaa+aaaa
+n1=int("%s" %(a))
+n2=int("%s%s"%(a,a))
+n3=int("%s%s%s"%(a,a,a))
+n4=int("%s%s%s%s"%	(a,a,a,a))
+result=n1+n2+n3+n4
+print("output= {0}".format(result))
