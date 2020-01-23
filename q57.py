dfcdd27d7d5331cc9362364389fba0b9ec1c080c
mahimachouhan <mchouhan@bestpeers.com>
2020-01-23 17:32:30 +0530

    solution of 57th porblem

--- q57.py
+++ q57.py
@@ -1,5 +1,7 @@
-list1=[1,3,6,78,35,55] 
-list2=[12,24,35,24,88,120,155]
+list1=set([1,3,6,78,35,55])
+list2=set([12,24,35,24,88,120,155])
 list3=[]
-list3=list3.append(list1 and list2)
-list3
+#list3=list3.append(list1 and list2)
+list3=list1.intersection(list2)
+print(list3)
+#import pdb;pdb.set_trace()
