1c5fc406dd2feb8f6519be0d96a3c3d20c455574
mahimachouhan <mchouhan@bestpeers.com>
2020-01-23 16:54:56 +0530

    solution of 51th porblem

---
+++ q51.py
@@ -0,0 +1,10 @@
+class American():
+	print("BAse class American")
+class NewYorker(American):
+	print("Derived class NewYorker")
+
+myobj=American()
+Derived_obj=NewYorker()
+print(American)
+print(NewYorker)
+
