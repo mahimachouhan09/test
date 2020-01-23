59dd1a230b1b379cf6b80885f4f7e8c888d182cd
mahimachouhan <mchouhan@bestpeers.com>
2020-01-23 16:44:36 +0530

    solution of 29th porblem

---
+++ q29.py
@@ -0,0 +1,10 @@
+def sum(n1,n2):
+	sum= int(n1)+int(n2)
+	return sum
+
+#s1=str(input("Enter Integer in string form:"))
+#s2=str(input("Enter integer in string form:"))
+n1="21"
+n2="12"
+sum=sum(n1,n2)
+print("sum= {0}".format(sum))
